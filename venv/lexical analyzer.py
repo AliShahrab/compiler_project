36dda72a27f5c2aee30b0c0e5232b2c59762fef9
AliShahrab <ali.shahrab@gmail.com>
2019-04-16 17:29:28 +0430

    final commit for phase 1

--- venv/lexical analyzer.py
+++ venv/lexical analyzer.py
@@ -3,7 +3,6 @@ import re
 
 class Compiler:
     def __init__(self):
-        self.input_file = ''
         self.input = ''
         self.index = 0
         self.number_of_line = 1
@@ -14,62 +13,64 @@ class Compiler:
         self.all_error = []
 
         self.KEYWORD = ["if",
-               "else",
-               "void",
-               "int",
-               "while",
-               "break",
-               "continue",
-               "switch",
-               "default",
-               "case",
-               "return"]
+                        "else",
+                        "void",
+                        "int",
+                        "while",
+                        "break",
+                        "continue",
+                        "switch",
+                        "default",
+                        "case",
+                        "return"]
 
         self.SYMBOL = [',',
-              ':',
-              ';',
-              '{',
-              '}',
-              '(',
-              ')',
-              '[',
-              ']',
-              '+',
-              '-',
-              '*',
-              '<',
-              '=',
-              '==']
+                       ':',
+                       ';',
+                       '{',
+                       '}',
+                       '(',
+                       ')',
+                       '[',
+                       ']',
+                       '+',
+                       '-',
+                       '*',
+                       '<',
+                       '=',
+                       '==']
 
         self.WHITESPACE = [chr(32),
-                  chr(10),
-                  chr(11),
-                  chr(12),
-                  chr(13),
-                  chr(9)]
+                           chr(10),
+                           chr(11),
+                           chr(12),
+                           chr(13),
+                           chr(9)]
 
     def open_file(self, input_file):
-        self.input_file = open(input_file, "r")
+        self.input = open(input_file, "r").read()
         # print(self.input)
 
     def get_next_token(self):
         aux = ""
         if self.index >= len(self.input):
             return 0, 0, 0, self.number_of_line
-        while self.basic_check() and ((self.cheack_current_char() in self.WHITESPACE) or (self.cheack_current_char() == '/')):
+        while self.basic_check() and (
+                (self.check_current_char() in self.WHITESPACE) or (self.check_current_char() == '/')):
             aux += self.read_current_char()
             if aux == '/':
-                if self.cheack_current_char() == '/':
-                    while  self.basic_check() and self.cheack_current_char() != '\n':
+                if self.check_current_char() == '/':
+                    while self.basic_check() and self.check_current_char() != '\n':
                         aux += self.read_current_char()
                     aux = ''
                     if not self.basic_check():
                         return 0, 0, 0, self.number_of_line
                     self.read_current_char()
 
-                elif self.cheack_current_char() == '*':
-                    while self.basic_check() and\
-                            (not(self.basic_check() and (aux[len(aux) - 1] == '*') and (self.cheack_current_char() == '/'))):
+                elif self.check_current_char() == '*':
+                    while self.basic_check() and \
+                            (not (self.basic_check() and (aux[len(aux) - 1] == '*') and (
+                                    self.check_current_char() == '/'))):
                         aux += self.read_current_char()
                     aux = ''
                     if not self.basic_check():
@@ -78,85 +79,62 @@ class Compiler:
                 else:
                     return self.set_error(aux)
             else:
-                while self.basic_check() and self.cheack_current_char() in self.WHITESPACE:
+                while self.basic_check() and self.check_current_char() in self.WHITESPACE:
                     self.read_current_char()
                 aux = ''
 
-
-        if re.match("^[0-9]+$", self.cheack_current_char()):
-            while  self.basic_check() and (re.match("^[0-9]+$", self.cheack_current_char())):
+        if re.match("^[0-9]+$", self.check_current_char()):
+            while self.basic_check() and (re.match("^[0-9]+$", self.check_current_char())):
                 aux += self.read_current_char()
-                # print('yess')
-        elif re.match("^[A-Za-z]*$", self.cheack_current_char()):
+        elif re.match("^[A-Za-z]*$", self.check_current_char()):
             aux += self.read_current_char()
-            while  self.basic_check() and (re.match("^[A-Za-z0-9]+$", self.cheack_current_char())):
+            while self.basic_check() and (re.match("^[A-Za-z0-9]+$", self.check_current_char())):
                 aux += self.read_current_char()
                 # print(aux)
-        elif self.cheack_current_char() in self.SYMBOL:
-            if self.cheack_current_char() != '=':
+        elif self.check_current_char() in self.SYMBOL:
+            if self.check_current_char() != '=':
                 aux += self.read_current_char()
                 return aux, self.set_token_category(aux), self.index, self.number_of_line
             else:
                 aux += self.read_current_char()
-                if self.cheack_current_char() == '=':
+                if self.check_current_char() == '=':
                     aux += self.read_current_char()
                     return aux, self.set_token_category(aux), self.index, self.number_of_line
-                elif self.is_invalid_char(self.cheack_current_char()):
+                elif self.is_invalid_char(self.check_current_char()):
                     return self.set_error(aux), self.number_of_line
                 else:
                     return aux, self.set_token_category(aux), self.index, self.number_of_line
 
-
-        if self.basic_check() and self.is_invalid_char(self.cheack_current_char()):
+        if self.basic_check() and self.is_invalid_char(self.check_current_char()):
             return self.set_error(aux)
         else:
             return aux, self.set_token_category(aux), self.index, self.number_of_line
-
-
-            # if self.input[self.index] in self.SYMBOL:
-            #     if aux == "":
-            #         return self.input[self.index], "SYMBOL", self.index + 1
-            #     else:
-            #         return aux, category_aux, self.index
-            # elif self.input[self.index] in self.WHITESPACE:
-            #     if aux == "":
-            #         return self.input[self.index], "WHITESPACE", self.index + 1
-            #     else:
-            #         return aux, category_aux, self.index + 1
-            #
-            # aux += self.input[self.index]
-            # category_aux = self.set_token_category(aux)
-            #
-            # if category_aux == "KEYWORD" and ((self.input[self.index + 1] in self.WHITESPACE) or (self.input[self.index + 1] in self.SYMBOL)):
-            #     return aux, category_aux, self.index + 1
-            #
-            # elif category_aux is None:
-            #     return aux, category_aux, self.index + 1
-            #
-            # self.index += 1
 
     def set_error(self, aux):
         aux += self.read_current_char()
         return aux, 'ERROR', self.index, self.number_of_line
 
     def current_char_is_starter_for_parse(self, char):
-        return (char in self.SYMBOL) or (re.match("^[A-Za-z0-9]*$"), char)
+        return (char in self.SYMBOL) or (re.match("^[A-Za-z0-9]*$", char))
 
     def basic_check(self):
         return self.index < len(self.input)
+
     def read_current_char(self):
         self.index += 1
         if self.input[self.index - 1] == '\n':
             self.number_of_line += 1
         return self.input[self.index - 1]
 
-    def cheack_current_char(self):
+    def check_current_char(self):
         if not self.basic_check():
             return
         return self.input[self.index]
+
     def is_invalid_char(self, char):
         # print('**' + char + '**')
-        return not((char in self.SYMBOL) or (re.match("^[A-Za-z0-9]*$", char)) or (char in self.WHITESPACE) or (char is '/'))
+        return not ((char in self.SYMBOL) or (re.match("^[A-Za-z0-9]*$", char)) or (char in self.WHITESPACE) or (
+                    char is '/'))
 
     def set_token_category(self, token):
 
@@ -185,13 +163,8 @@ class Compiler:
         return category
 
     def process(self):
-
-        self.input = self.input_file.read()
-        # self.input = '!void!(main) !{start /end);== \n' \
-        #              'asdf 123 wr asdf2 12d'
         print(self.input)
         self.index = 0
-        number_if_line = 0
         while True:
             token, token_category, self.index, number_if_line = self.get_next_token()
             if token == token_category == self.index == 0:
@@ -199,7 +172,7 @@ class Compiler:
             if token_category != 'ERROR':
                 self.all_token.append([token_category, token, number_if_line])
             else:
-                self.all_error.append([token_category, token, number_if_line])
+                self.all_error.append(['invalid input', token, number_if_line])
             # print(token, token_category, self.index)
 
     def show_tokens(self):
@@ -222,27 +195,47 @@ class Compiler:
             if counter < len(self.all_error) and j == self.all_error[counter][self.INDEX_OF_NUMBER_OF_LINE]:
                 print(str(j) + '. ', end='')
                 while counter < len(self.all_error) and j == self.all_error[counter][self.INDEX_OF_NUMBER_OF_LINE]:
-                    print('(' + str(self.all_error[counter][self.INDEX_OF_CATEGORY]) + ', ' +
-                          str(self.all_error[counter][self.INDEX_OF_TOKEN]) + ')', end='')
+                    print('(' + str(self.all_error[counter][self.INDEX_OF_TOKEN]) + ', ' +
+                          str(self.all_error[counter][self.INDEX_OF_CATEGORY]) + ')', end='')
                     counter += 1
                 print()
 
+    def export_token(self):
+        counter = 0
+        output = ''
+        for i in range(self.number_of_line):
+            j = i + 1
+            if counter < len(self.all_token) and j == self.all_token[counter][self.INDEX_OF_NUMBER_OF_LINE]:
+                output += str(j) + '. '
+                while counter < len(self.all_token) and j == self.all_token[counter][self.INDEX_OF_NUMBER_OF_LINE]:
+                    output += '(' + str(self.all_token[counter][self.INDEX_OF_CATEGORY]) + ', ' + \
+                              str(self.all_token[counter][self.INDEX_OF_TOKEN]) + ')'
+                    counter += 1
+                output += '\n'
+        writer = open('scanner.txt', 'w')
+        writer.write(output)
 
-my_comiler = Compiler()
-my_comiler.open_file('inp.txt')
+    def export_error(self):
+        counter = 0
+        output = ''
+        for i in range(self.number_of_line):
+            j = i + 1
+            if counter < len(self.all_error) and j == self.all_error[counter][self.INDEX_OF_NUMBER_OF_LINE]:
+                output += str(j) + '. '
+                while counter < len(self.all_error) and j == self.all_error[counter][self.INDEX_OF_NUMBER_OF_LINE]:
+                    output += '(' + str(self.all_error[counter][self.INDEX_OF_TOKEN]) + ', ' + \
+                              str(self.all_error[counter][self.INDEX_OF_CATEGORY]) + ')'
+                    counter += 1
+                output += '\n'
+        writer = open('lexical_errors.txt', 'w')
+        writer.write(output)
 
-my_comiler.process()
-my_comiler.show_tokens()
-my_comiler.show_error()
 
+my_compiler = Compiler()
+my_compiler.open_file('inp.txt')
 
-#
-# print(set_token_category("92342348"))
-# print(set_token_category(";"))
-# print(set_token_category("sdSSSD2"))
-# print(set_token_category("@#$"))
-# print(set_token_category("//"))
-# print(set_token_category("/*"))
-# print(set_token_category("else"))
-# print(set_token_category(" "))
-
+my_compiler.process()
+my_compiler.show_tokens()
+my_compiler.show_error()
+my_compiler.export_token()
+my_compiler.export_error()
